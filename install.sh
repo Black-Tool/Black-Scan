df4391a6dc4c2a80c112d16eaa51eece73479353
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-29 11:46:42 -0400

    Black

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# This code write by Mr.nope
+if [[ "$(id -u)" -ne 0 ]]; then
+  echo ""
+  echo "Please, Run This Programm as Root!"
+  echo ""
+  exit 1
+fi
+clear
+echo "Installing..."
+sleep 2
+apt install python
+apt install python3
+chmod a+x black
+echo ""
+echo "Installing..., Finish...!"
+echo ""
+exit 1
